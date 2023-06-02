df9f233ab58bf5e772c36d42b948b1858ca82dc1
Cláudio Rapôso <engcfraposo@gmail.com>
2023-06-02 06:58:29 -0300

    port

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   swagger(app);
-  await app.listen(3000);
+  await app.listen(8080);
 }
 bootstrap();
